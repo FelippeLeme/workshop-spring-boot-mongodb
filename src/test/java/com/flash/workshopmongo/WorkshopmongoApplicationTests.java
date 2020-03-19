feb480b75b6f266e68e385af8e1aa91ad2e2be1d
Felippe <felippe.leme@gmail.com>
2020-03-19 15:51:30 -0300

    Primeiro comit

---
+++ src/test/java/com/flash/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flash.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
